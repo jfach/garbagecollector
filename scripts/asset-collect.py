082d12a0d00dc932fdcd6f3311e6000e7d7e6d5d
sometheycallme <tim@glidegames.com>
2015-03-31 16:23:44 -0400

    add csv file handler for asset-collection

--- scripts/asset-collect.py
+++ scripts/asset-collect.py
@@ -1,18 +1,30 @@
+
 #!/usr/bin/env python
 # vim: set ts=2 sw=2 ai et:
 # 
+# http://stackoverflow.com/questions/10373247/how-do-i-write-a-python-dictionary-to-a-csv-file
+
 import nmapTools
 import dnsTools
 import csv
+import os
 
-# replace IP Range with range of your choosing
-results = nmapTools.ping_sweep('192.168.1.0/24')
+results = nmapTools.ping_sweep('6.3.3.0/24')
 
-# dnsTools uses your local machines DNS servers to run query
 for x in results:
-        #print(x)
-        for y in results[x]:
-                #print (y,':',results[x][y])
-                dns_rec = dnsTools.lookup(y)
-                if dns_rec[2] is not None:
-                        print (dns_rec[2],dns_rec[0],dns_rec[0])
+  for y in results[x]:
+    dns_rec = dnsTools.lookup(y)
+    # uncomment to test
+    #print (y,dns_rec[0],dns_rec[1],dns_rec[2])
+    if dns_rec[2] is not None:
+        # uncomment to test
+        # print (dns_rec[2])
+        dns_dict = {'ip':dns_rec[2], 'fqdn': dns_rec[0], 'alias': dns_rec[1]}
+        # uncomment to test
+	# print dns_dict
+
+
+with open('test.csv', 'wb') as f:
+  w = csv.DictWriter(f,dns_dict.keys())
+  for data in dns_dict:
+    w.writerow(dns_dict)
