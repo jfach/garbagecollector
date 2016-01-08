7b4e362ef10967abaca693c4d2c17bfc5bbbc276
Jordan Facibene <jordan.facibene13@stjohns.edu>
2016-01-08 13:45:43 -0500

    fix setup script script now pulls correct package + uses correct tar flags

--- scripts/get-nmap-package.sh
+++ scripts/get-nmap-package.sh
@@ -1,7 +1,7 @@
-curl -O http://xael.org/norman/python/python-nmap/python-nmap-0.3.4.tar.gz
+curl -O http://xael.org/pages/python-nmap-0.5.0-1.tar.gz
 
-tar xvzf python-nmap-0.3.4.tar.gz
+tar -xvf python-nmap-0.5.0-1.tar.gz
 
-cd python-nmap-0.3.4
+cd python-nmap-0.5.0-1
 python setup.py install
 
